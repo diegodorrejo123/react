3796686e898638208a30ee916c9b5d0785b1025a
Diego Dorrejo <ddorrejo@itla.edu.do>
2021-09-19 23:06:12 -0400

    End First Section

---
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button, Icon, Item, ItemContent, ItemDescription, ItemGroup, ItemHeader, ItemImage, List, Segment, SegmentGroup } from 'semantic-ui-react';
+import EventListAttendee from './EventListAttendee';
+
+export default function EventListItem({event}){
+    return (
+        <SegmentGroup>
+            <Segment>
+                <ItemGroup>
+                    <Item>
+                        <ItemImage size='tiny' circular src={event.hostPhotoURL} />
+                        <ItemContent>
+                            <ItemHeader content={event.title} />
+                            <ItemDescription>
+                                Hosted By {event.hostedBy}
+                            </ItemDescription>
+                        </ItemContent>
+                    </Item>
+                </ItemGroup>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {event.date}
+                    <Icon name='marker' /> {event.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                <List horizontal>
+                    {event.attendees.map(attendee => (
+                        <EventListAttendee key={attendee.id} attendee={attendee} />
+                    ))}
+                </List>
+            </Segment>
+            <Segment clearing>
+                <div>{event.description}</div>
+                <Button color='teal' floated='right' content='View' />
+            </Segment>
+        </SegmentGroup>
+    )
+}
