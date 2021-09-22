6d5b5ce2cb499e51fb4971656479a851f601da44
Diego Dorrejo <ddorrejo@itla.edu.do>
2021-09-22 13:27:42 -0400

    End of section 2

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,19 +1,43 @@
-import React, {useState} from 'react';
-import { Grid, GridColumn } from 'semantic-ui-react';
-import EventForm from '../eventForm/EventForm';
-import EventList from './EventList';
+import React, { useState } from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import EventForm from "../eventForm/EventForm";
+import EventList from "./EventList";
 import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard({formOpen, setFormOpen}) {
-    const [events, setEvents] = useState(sampleData);
-    return (
-        <Grid>
-            <GridColumn width={10}>
-                <EventList events={events} />
-            </GridColumn>
-            <GridColumn width={6}>
-                { formOpen && <EventForm setFormOpen={setFormOpen} /> }
-            </GridColumn>
-        </Grid>
-    ) 
+export default function EventDashboard({ formOpen, setFormOpen, selectEvent, selectedEvent }) {
+  const [events, setEvents] = useState(sampleData);
+
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+  function handleUpdateEvent(updatedEvent){
+      setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+      selectEvent(null);
+  }
+
+  function handleDeleteEvent(eventId){
+      setEvents(events.filter(evt => evt.id !== eventId))
+  }
+  return (
+    <Grid>
+      <GridColumn width={10}>
+        <EventList events={events} 
+        selectEvent={selectEvent}
+        deleteEvent={handleDeleteEvent}
+        />
+      </GridColumn>
+      <GridColumn width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+            updateEvent={handleUpdateEvent}
+          />
+        )}
+      </GridColumn>
+    </Grid>
+  );
 }
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,32 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormField, Header, Segment } from "semantic-ui-react";
+import cuid from "cuid";
 
-export default function EventForm({setFormOpen}){
-    return (
-        <Segment clearing>
-            <Header content='Create new event' />
-            <Form>
-                <FormField>
-                    <input type="text" placeholder='Event title' />
-                </FormField>
-                <FormField>
-                    <input type="text" placeholder='Category' />
-                </FormField>
-                <FormField>
-                    <input type="text" placeholder='Description' />
-                </FormField>
-                <FormField>
-                    <input type="text" placeholder='City' />
-                </FormField>
-                <FormField>
-                    <input type="text" placeholder='Venue' />
-                </FormField>
-                <FormField>
-                    <input type="date" placeholder='Date' />
-                </FormField>
-                <Button type='submit' floated='right' positive content='Submit' />
-                <Button onClick={() => setFormOpen(false)} type='submit' floated='right' content='Cancel' />
-            </Form>
-        </Segment>
-    )
+export default function EventForm({
+  setFormOpen,
+  setEvents,
+  createEvent,
+  selectedEvent,
+  updateEvent,
+}) {
+  const initialValues = selectedEvent ?? {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  function handleFormSubmit() {
+    selectedEvent
+      ? updateEvent({ ...selectedEvent, ...values })
+      : createEvent({
+          ...values,
+          id: cuid(),
+          hostedBy: "Bob",
+          attendees: [],
+          hostPhotoURL: "/assets/user.png",
+        });
+    setFormOpen(false);
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
+
+  return (
+    <Segment clearing>
+      <Header content={selectedEvent ? "Edit the event" : "Create new event"} />
+      <Form onSubmit={handleFormSubmit}>
+        <FormField>
+          <input
+            type="text"
+            placeholder="Event title"
+            name="title"
+            value={values.title}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </FormField>
+        <FormField>
+          <input
+            type="text"
+            placeholder="Category"
+            name="category"
+            value={values.category}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </FormField>
+        <FormField>
+          <input
+            type="text"
+            placeholder="Description"
+            name="description"
+            value={values.description}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </FormField>
+        <FormField>
+          <input
+            type="text"
+            placeholder="City"
+            name="city"
+            value={values.city}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </FormField>
+        <FormField>
+          <input
+            type="text"
+            placeholder="Venue"
+            name="venue"
+            value={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </FormField>
+        <FormField>
+          <input
+            type="date"
+            placeholder="Date"
+            name="date"
+            value={values.date}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </FormField>
+        <Button type="submit" floated="right" positive content="Submit" />
+        <Button
+          onClick={() => setFormOpen(false)}
+          type="submit"
+          floated="right"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
 }
